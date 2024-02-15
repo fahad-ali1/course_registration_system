ee673c141cf25daf70743f7dcab7832e2dcf3d62
Fahad <97869609+fahad-ali1@users.noreply.github.com>
2024-02-15 13:23:06 -0700

    clean up routes

--- be/routes/route.js
+++ be/routes/route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { getAllCourses, getCourse, registerCourse, unregisterCourse, getRegisteredCourses } from "../controllers/course.controller.js";
-import { getAllStudents, getStudent } from "../controllers/student.controller.js";
+import { getAllCourses, getCourse, registerCourse, unregisterCourse } from "../controllers/course.controller.js";
+import { getAllStudents, getStudent, getRegisteredCourses } from "../controllers/student.controller.js";
 const router = express.Router();
 
 // Courses
@@ -13,7 +13,5 @@ router.get('/:studentID/courses', getRegisteredCourses);
 // Students
 router.get('/students', getAllStudents);
 router.get('/students/:studentID', getStudent);
-// router.post('/students/:studentId/courses/enroll', enrollCourse);
-// router.post('/students/:studentId/courses/unenroll', SunenrollCourse);
 
 export default router;
